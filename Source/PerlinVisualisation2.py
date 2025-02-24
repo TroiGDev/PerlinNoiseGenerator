30dec7afd436abd62231171b1b68be9bd3557478
Troi <143833120+TroiGDev@users.noreply.github.com>
2025-02-24 09:55:51 +0100

    Updated PerlinVisualisation2.py
    added clouds and waves

--- Source/PerlinVisualisation2.py
+++ Source/PerlinVisualisation2.py
@@ -1,5 +1,9 @@
 import pygame
+import math
 import Perlin
+
+from pyRecorder import Recorder
+recorder = Recorder("gameoflife")
 
 worldWidth = 100
 worldHeight = 100
@@ -11,12 +15,13 @@ screenHeight = worldHeight * drawnCellSize
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('PerlinVisualisation')
 
-#remove window icon
-transparent_surface = pygame.Surface((32, 32), pygame.SRCALPHA)
-transparent_surface.fill((0, 0, 0, 0))
-pygame.display.set_icon(transparent_surface)
+#cloud transparent surface
+cloudSurface = pygame.Surface((worldWidth, worldHeight), pygame.SRCALPHA)
+
 
 def drawWorld(world):
+    cloudSurface.fill((0, 0, 0, 0))
+
     #draw squares where there is a 1 in the world array
     for x in range(worldWidth):
         for y in range(worldHeight):
@@ -33,6 +38,16 @@ def drawWorld(world):
             #using float intensity
             pygame.draw.rect(screen, color, (x * drawnCellSize, y * drawnCellSize, drawnCellSize, drawnCellSize))
 
+            #draw clouds perlin
+            cx = math.trunc((x + cloudOffsetx)) % worldWidth
+            cy = math.trunc((y + cloudOffsety)) % worldHeight
+            if cloudNoise[x][y] > cloudThreshold:
+                pygame.draw.rect(cloudSurface, (255, 255, 255, 60), pygame.Rect(cx, cy, drawnCellSize, drawnCellSize))
+
+    scaledClouds = pygame.transform.scale(cloudSurface, (worldWidth * drawnCellSize, worldHeight * drawnCellSize))  # Scale to match screen size
+
+    screen.blit(scaledClouds, (0, 0))
+
 #stacking 2 perlin noises
 params = [
     (10, 10, "fade"),
@@ -40,8 +55,24 @@ params = [
 ]
 world = Perlin.fractalStackedPerlin(worldWidth, worldHeight, params, 100)
 
-thresholds = [40, 55, 60, 65, 70, 80, 85, 90, 93, 96]
-colors = [(0, 75, 155), (0, 170, 220), (250, 210, 130), (230, 180, 60), (20, 150, 0), (40, 100, 0), (80, 80, 80), (60, 60, 60), (220, 220, 220), (255, 255, 255)]
+wave = 55
+waveMag = 1.5
+waveFreq = 0.01
+waveIter = 0
+
+cloudSpeedx = 0.008
+cloudSpeedy = 0.002
+cloudOffsetx = 0
+cloudOffsety = 0
+cloudThreshold = 0.8
+cloudMag = 0.0001
+cloudFreq = 0.01
+cloudIter = 0
+
+cloudNoise = Perlin.perlin(worldWidth, worldHeight, 5, 1, "fade")
+
+thresholds = [40, wave, 60, 65, 70, 80, 90, 95]
+colors = [(0, 75, 155), (0, 170, 220), (250, 210, 130), (230, 180, 60), (0, 150, 80), (0, 100, 50), (100, 100, 100), (150, 150, 150)]
 
 running = True
 while running:
@@ -53,11 +84,27 @@ while running:
     # Fill screen
     screen.fill((0, 0, 0))
 
+    #update wave sin threshold
+    waveIter += 1
+    thresholds[1] = wave + math.sin(waveIter * waveFreq) * waveMag
+
+    #udpate cloud pos
+    cloudOffsetx += cloudSpeedx
+    cloudOffsety += cloudSpeedy
+    cloudIter += 1
+    cloudThreshold = cloudThreshold + math.sin(cloudIter * cloudFreq) * cloudMag
+
     #draw world
     drawWorld(world)
 
     # Update the display
     pygame.display.flip()
+
+    #after rendered frame, take recorder shot
+    recorder.takeShot(screen)
+
+#get video from taken shots
+recorder.getVideo("GOL_Plants.avi", 24)
 
 # Quit Pygame
 pygame.quit()
